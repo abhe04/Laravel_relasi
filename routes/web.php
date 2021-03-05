b6f8f6e2756755f92e3f5b40d22d2eba962fcc8c
abhe04 <70308024+abhe04@users.noreply.github.com>
2021-03-05 10:38:54 +0700

    Bagian 03 - Membuat Read Data User

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,28 @@ Route::get('/create_user_profile', function() {
 
     return $user;
 });
+
+Route::get('/read_user', function () {
+    $user = User::find(1);
+
+    $data = [
+        'name' => $user->name,
+        'phone' => $user->profile->phone,
+        'address' => $user->profile->address
+    ];
+
+    return $data;
+});
+
+Route::get('read_profile', function () {
+    $profile = Profile::where('phone', '0892123124151')->first();
+
+    $data = [
+        'name' => $profile->user->name,
+        'email' => $profile->user->email,
+        'phone' => $profile->phone,
+        'address' => $profile->address
+    ];
+
+    return $data;
+});
